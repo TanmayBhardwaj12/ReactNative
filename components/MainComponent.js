d60f7b256845ffc9a27d6f9b3017e92c05c21c06
Tanmay Bhardwaj <tanmaybharadwaj484@gmail.com>
2020-07-10 16:58:31 +0530

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator  } from 'react-navigation-stack';
+import { NavigationContainer} from '@react-navigation/native';
 import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Icon } from 'react-native-elements';
 import { Constants } from 'expo-constants';
 
 const MenuNavigator = createStackNavigator({
@@ -27,14 +30,37 @@ const MenuNavigator = createStackNavigator({
 
 const MenuScreen = createAppContainer(MenuNavigator);
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const HomeScreen = createAppContainer(HomeNavigator);
+
+const Drawer = createDrawerNavigator();
+
 class Main extends Component {
 
   render() {
  
     return (
-        <View style={{flex:1 }}>
-            <MenuScreen />
-        </View>
+        <NavigationContainer>
+            <View style={{ flex: 1 }}>
+            <Drawer.Navigator initialRouteName="Home" drawerStyle={{backgroundColor: '#D1C4E9'}}>
+                <Drawer.Screen name="Home" component={()=><HomeScreen/>} />
+                <Drawer.Screen name="Menu" component={()=><MenuScreen/>} />
+            </Drawer.Navigator>
+            </View>
+        </NavigationContainer>
     );
   }
 }
